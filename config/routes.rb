288f01a98a4da0238f7927f6edccc6e2ef2dcf1b
Scott Firestone <scott.firestone.dev@gmail.com>
2016-06-06 12:59:09 -0600

    Added buttons and controller logic for marking links as read and unread

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   delete "logout", to: "sessions#destroy"
 
   resources :users, only: [:new, :create]
-  resources :links, only: [:index, :create]
+  resources :links, only: [:index, :create, :update]
 end
---
+++ spec/features/user_marks_link_as_read_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.feature "User marks link as read" do
+  scenario "the link is then marked as read" do
+    user = User.create(email: "email@example.com", password: "password")
+    link = user.links.create(url: "http://www.google.com", title: "google")
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    expect(link.read).to eq(false)
+
+    visit links_path
+
+    click_on "Mark as Read"
+
+    expect(current_path).to eq(links_path)
+
+    expect(page).to have_button("Mark as Unread")
+    expect(page).to_not have_button("Mark as Read")
+  end
+end
