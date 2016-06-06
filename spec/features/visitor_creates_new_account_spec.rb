8fac9e28e85807d4f30109c0c46abad80ff493cd
Scott Firestone <scott.firestone.dev@gmail.com>
2016-06-06 09:59:56 -0600

    Create users controller and new user form view

--- spec/features/visitor_creates_new_account_spec.rb
+++ spec/features/visitor_creates_new_account_spec.rb
@@ -1,13 +1,17 @@
 require "rails_helper"
 
-RSpec.feature "User can register for account" do
+RSpec.feature "User creates new account" do
   scenario "they are redirected to links index page" do
-    User.create(email: "email@example.com", password: "password")
     visit login_path
+    click_on "Register"
+
+    expect(current_path).to eq(new_user_path)
 
     fill_in "Email", with: "email@example.com"
     fill_in "Password", with: "password"
-    click_on "Login"
+    fill_in "Password confirmation", with: "password"
+
+    click_on "Register"
 
     expect(current_path).to eq(links_path)
   end
