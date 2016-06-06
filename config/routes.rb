b2730fd062246ea45b91fbe5b02b710735a94ef1
Scott Firestone <scott.firestone.dev@gmail.com>
2016-06-06 09:39:23 -0600

    Create users
    Users model with email and secure password
    Route for new user path

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: "links#index"
 
   get "login", to: "sessions#new"
+
+  resources :users, only: [:new]
 end
--- spec/features/root_has_sign_in_or_up_spec.rb
+++ spec/features/root_has_sign_in_or_up_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "Root path points to signing in or up" do
     visit root_path
 
     expect(current_path).to eq(login_path)
-    expect(page).to have_link("Login")
+    expect(page).to have_button("Login")
     expect(page).to have_link("Register")
   end
 end
